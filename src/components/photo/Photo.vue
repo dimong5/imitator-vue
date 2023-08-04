756edfadfa5bc339c10d80b48bb5482f0d7c6ccd
dmitry <grigorievd@fizpribor.ru>
2023-08-04 14:12:54 +0300

    initial commit

---
+++ src/components/photo/Photo.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-col cols="4">
+    <v-card>
+      <v-card-title>
+        {{ photo.title }}
+      </v-card-title>
+      <v-card-text>
+        <v-img wigth="200" height="200" :src="photo.url" />
+      </v-card-text>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    photo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/PhotosPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <PhotoForm @addPhoto="addPhoto" />
+    <v-row>
+      <Photo v-for="photo in photos" :photo="photo" />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Photo from "@/components/photo/Photo";
+import PhotoForm from "@/components/photo/PhotoForm";
+
+export default {
+  components: {
+    Photo,
+    PhotoForm,
+  },
+  data: () => ({
+    photos: [],
+  }),
+  mounted() {
+    this.fetchTodo();
+  },
+  methods: {
+    fetchTodo() {
+      this.axios
+        .get("https://jsonplaceholder.typicode.com/photos?_limit=10")
+        .then((response) => (this.photos = response.data));
+    },
+    addPhoto(photo) {
+      this.photos.push(photo);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
